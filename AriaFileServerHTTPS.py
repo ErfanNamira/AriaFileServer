b7757357aa4d79e16ae6c41116a5918a619e54da
Namira <115206959+ErfanNamira@users.noreply.github.com>
2023-09-14 09:32:22 +0330

    Add files via upload

---
+++ AriaFileServerHTTPS.py
@@ -0,0 +1,66 @@
+# Aria File Server HTTPS Version
+# HTTPS File Server with Authentication
+# ErfanNamira
+# https://github.com/ErfanNamira/AriaFileServer
+
+# sudo apt update
+# sudo apt install certbot python3-pip python3-certbot-nginx
+# pip3 install flask
+# sudo certbot --nginx -d sub.domain.com
+# sudo ufw allow 443/tcp
+# sudo python3 AriaFileServerHTTPS.py
+
+from flask import Flask, request, Response
+from functools import wraps
+import os
+
+app = Flask(__name__)
+
+# Define a dictionary to store username-password pairs (You should use a proper database in production)
+users = {
+    'aria': 'ariapass',
+    'sara': 'sarapass',
+}
+
+# Function to check if a given username and password are valid
+def is_valid_user(username, password):
+    return username in users and users[username] == password
+
+# Decorator to require authentication for specific routes
+def require_auth(func):
+    @wraps(func)
+    def auth_wrapper(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not is_valid_user(auth.username, auth.password):
+            return Response('Authentication failed', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        return func(*args, **kwargs)
+    return auth_wrapper
+
+# Route for the root URL ("/") - asks for authentication
+@app.route('/')
+@require_auth
+def index():
+    # Change the welcome message here
+    welcome_message = 'Welcome to the Aria File Server!'
+    
+    # Get the list of files in the current directory
+    file_list = os.listdir('.')
+    file_list_html = '<ul>'
+    for filename in file_list:
+        file_list_html += f'<li><a href="/files/{filename}">{filename}</a></li>'
+    file_list_html += '</ul>'
+    return f'{welcome_message}<br>List of files in the directory:<br>{file_list_html}'
+
+# Simple file server route
+@app.route('/files/<path:filename>')
+@require_auth
+def serve_file(filename):
+    try:
+        with open(filename, 'rb') as f:
+            return Response(f.read(), mimetype='application/octet-stream')
+    except FileNotFoundError:
+        return 'File not found', 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2053, debug=True, ssl_context=('path_to_your_cert.pem', 'path_to_your_privkey.pem'))
+
