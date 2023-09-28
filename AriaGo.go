15758933b6210e898c6bf84b58be5e081ca80405
Namira <115206959+ErfanNamira@users.noreply.github.com>
2023-09-28 12:42:23 +0330

    Update AriaGo.go

--- AriaGo.go
+++ AriaGo.go
@@ -6,21 +6,18 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
-	"os"
-	"log"
-	"strings"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"sort"
 	"strconv"
-	"net"
-	"encoding/base64"
+	"strings"
 	"time"
-	"github.com/sssvip/qrterminal"
 )
 
 var staticPath = "/static/"
-var repoUrl = "https://github.com/ErfanNamira/AriaFileServer"
 
 var (
 	// Define a map to store username-password pairs
@@ -80,7 +77,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 		} else {
 			fmt.Fprintln(w, "<p>Directory listing is disabled.</p>")
 		}
-		fmt.Fprintf(w, "</br></br></br>See the new version of AriaFileServer :<a href=\"%s\">%s</a>", repoUrl, repoUrl)
 	} else {
 		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
 		http.Error(w, "Unauthorized access", http.StatusUnauthorized)
@@ -93,46 +89,8 @@ func isValidUser(username, password string) bool {
 	return ok && password == storedPassword
 }
 
-func getLocalIPs() []string {
-	result := []string{"localhost"}
-	addrSlice, err := net.InterfaceAddrs()
-	if err != nil {
-		log.Println("Error getting local IP addresses:", err)
-		return result
-	}
-	for _, addr := range addrSlice {
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				result = append(result, ipnet.IP.String())
-			}
-		}
-	}
-	return result
-}
-
 func entry() {
 	port := config.Port
-	showQRCode := false
-	const qr = "qr"
-	showQRCodeIpFilter := qr
-
-	if len(os.Args) > 1 {
-		tmpPort, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			log.Println("HTTP port must be a number, e.g., AriaFileServer.exe 8080")
-			log.Println("Using default HTTP port:" + port)
-		} else {
-			port = strconv.Itoa(tmpPort)
-		}
-		// Check for QR code generation
-		for _, arg := range os.Args {
-			if strings.Contains(arg, qr) {
-				showQRCode = true
-				showQRCodeIpFilter = arg
-				break
-			}
-		}
-	}
 
 	http.HandleFunc("/", home)
 	http.HandleFunc(staticPath, func(w http.ResponseWriter, r *http.Request) {
@@ -156,18 +114,6 @@ func entry() {
 			urlAddr = fmt.Sprintf("%s:%s", urlAddr, port)
 		}
 		log.Println(fmt.Sprintf("%s%s", optPreAddr, urlAddr))
-		if showQRCode {
-			if showQRCodeIpFilter != qr {
-				if strings.Contains(urlAddr, strings.Replace(showQRCodeIpFilter, qr, "", -1)) {
-					qrterminal.Generate(urlAddr, qrterminal.L, os.Stdout)
-					fmt.Println()
-					break
-				}
-			} else {
-				qrterminal.Generate(urlAddr, qrterminal.L, os.Stdout)
-				fmt.Println()
-			}
-		}
 	}
 
 	var serverAddr string
